d2b29a3cca763669120a13ae5198902733b8d06b
danamestrial <chayapolbun@gmail.com>
2023-10-27 23:57:26 +0700

    feat: CORS configured, m3u8 now get presigned url instead of cdns

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import base64
 from fastapi import FastAPI, HTTPException, status
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from app.middleware.middleware import MyMiddleware
 
 from app.utils.auth import OAuth2PasswordBearerWithCookie
 from fastapi.security.utils import get_authorization_scheme_param
-from .routers import test, getData, auth, videoUpload
+from .routers import m3u8, getData, auth, videoUpload
+from fastapi.middleware.cors import CORSMiddleware
 
 from typing import Annotated, Any
 
@@ -13,8 +15,17 @@ from fastapi import Depends, FastAPI, Request, Response
 
 app = FastAPI()
 
-app.include_router(test.router)
+app.include_router(m3u8.router)
 app.include_router(auth.router)
 app.include_router(videoUpload.router)
 app.include_router(getData.router)
 # app.add_middleware(MyMiddleware)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
---
+++ app/routers/m3u8.py
@@ -0,0 +1,98 @@
+import os
+import boto3
+import botocore
+
+from fastapi import APIRouter, Response
+from os.path import isfile
+from mimetypes import guess_type
+from dotenv import load_dotenv
+
+load_dotenv()
+
+router = APIRouter(prefix="/api/m3u8")
+
+
+@router.get("/request_presigned/{path}/{filename}")
+async def get_legacy_data(path: str, filename: str):
+    fullpath = './static/' + path + "/" + filename
+
+    ## Create new buffer folder if it doesnt exist
+    if not os.path.exists('static/' + path):
+        os.makedirs('static/' + path)
+
+    session = boto3.session.Session()
+    client = session.client('s3',
+                            config=botocore.config.Config(s3={'addressing_style': 'virtual'}), ## Configures to use subdomain/virtual calling format.
+                            region_name='sgp1',
+                            endpoint_url='https://sgp1.digitaloceanspaces.com',
+                            aws_access_key_id=os.getenv('SPACES_KEY'),
+                            aws_secret_access_key=os.getenv('SPACES_SECRET'))
+
+    client.download_file("toktik-s3-videos", path + "/" + filename, fullpath)
+
+    if not isfile(fullpath):
+        return Response(status_code=404)
+
+    # Read the file content
+    with open(fullpath, 'r') as file:
+        lines = file.readlines()
+
+    # Iterate through the lines and modify as needed
+    with open(fullpath, 'w') as file:
+        for line in lines:
+            if line.strip().endswith('.ts'):
+                url = client.generate_presigned_url(ClientMethod='get_object',
+                                        Params={'Bucket': 'toktik-s3-videos',
+                                                'Key': f"{path.strip()}/{line.strip()}"},
+                                        ExpiresIn=80)
+
+                file.write(url + "\n")
+            else:
+                file.write(line)
+
+    with open(fullpath) as f:
+        content = f.read()
+
+    content_type, _ = guess_type(fullpath)
+    return Response(content, media_type=content_type)
+
+@router.get("/static/{path}/{filename}")
+async def get_site(path, filename):
+    fullpath = './static/' + path + "/" + filename
+
+    ## Create new buffer folder if it doesnt exist
+    if not os.path.exists('./static/' + path):
+        os.makedirs('./static/' + path)
+
+    session = boto3.session.Session()
+    client = session.client('s3',
+                            config=botocore.config.Config(s3={'addressing_style': 'virtual'}), ## Configures to use subdomain/virtual calling format.
+                            region_name='sgp1',
+                            endpoint_url='https://sgp1.digitaloceanspaces.com',
+                            aws_access_key_id=os.getenv('SPACES_KEY'),
+                            aws_secret_access_key=os.getenv('SPACES_SECRET'))
+
+    client.download_file("toktik-s3-videos", path + "/" + filename , fullpath)
+
+    if not isfile(fullpath):
+        return Response(status_code=404)
+
+    # Read the file content
+    with open(fullpath, 'r') as file:
+        lines = file.readlines()
+
+    # Iterate through the lines and modify as needed
+    with open(fullpath, 'w') as file:
+        for line in lines:
+            if line.strip().endswith('.m3u8'):
+                # Add "good_" in front of the line
+                new_line = router.prefix + '/request_presigned/' + path + '/' + line
+                file.write(new_line)
+            else:
+                file.write(line)
+
+    with open(fullpath) as f:
+        content = f.read()
+
+    content_type, _ = guess_type(fullpath)
+    return Response(content, media_type=content_type)
